fd17819aa574a7ff1527189dc338d07e32d4d75a
PrathmTayade <122280972+PrathmTayade@users.noreply.github.com>
2023-05-02 22:38:17 +0530

    fixed imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Layout from "./pages/Layout";
 import ContactsPage from "./pages/ContactsPage";
 
 function App() {
